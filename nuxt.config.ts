471b732c35ebaec910479f989c081f351fbf64c5
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-12-17 13:42:55 +0700

    Y

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      link: [{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css'}],
+    }
+  }
 })
